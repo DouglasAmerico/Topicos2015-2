8937514bbee0310fab837c92a16e7bf48505feee
Douglas Americo <douglas.alex.1212@gmail.com>
2015-08-03 21:05:07 -0300

    Implementação da inicialização do Jogo

--- SpaceInvaders/core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
+++ SpaceInvaders/core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
@@ -1,12 +1,27 @@
 package br.grupointegrado.SpaceInvaders;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+
 
 /**
  * Created by Douglas on 03/08/2015.
  */
 public class TelaJogo extends TelaBase {
+
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Stage palco;
+    private BitmapFont fonte;
+    private Label lbPontuacao;
+
     /**
      * Contrutor Padrão da Tela de Jogo
      * @param game Referencia para a classe principal
@@ -15,11 +30,29 @@ public class TelaJogo extends TelaBase {
         super(game);
     }
 
+    private void initfonte(){
+        fonte = new BitmapFont();
+    }
+
+    private void initinformacoes(){
+        Label.LabelStyle lbStilo = new Label.LabelStyle();
+        lbStilo.font = fonte;
+        lbStilo.fontColor = Color.WHITE;
+
+        lbPontuacao = new Label("0 pontos", lbStilo);
+        palco.addActor(lbPontuacao);
+    }
     /**
      * Chamado quando a tela é exebida
      */
     @Override
     public void show() {
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        batch = new SpriteBatch();
+        palco = new Stage(new FillViewport(camera.viewportWidth,camera.viewportHeight,camera));
+
+        initfonte();
+        initinformacoes();
     }
 
     /**
@@ -30,6 +63,9 @@ public class TelaJogo extends TelaBase {
     public void render(float delta) {
         Gdx.gl.glClearColor(.15f,.15f,25f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        lbPontuacao.setPosition(10, camera.viewportHeight - 20);
+        palco.act(delta);
+        palco.draw();
     }
 
     /**
@@ -39,6 +75,8 @@ public class TelaJogo extends TelaBase {
      */
     @Override
     public void resize(int width, int height) {
+        camera.setToOrtho(false,width,height);
+        camera.update();
     }
 
     /**
@@ -60,5 +98,8 @@ public class TelaJogo extends TelaBase {
      */
     @Override
     public void dispose() {
+        batch.dispose();
+        palco.dispose();
+        fonte.dispose();
     }
 }
