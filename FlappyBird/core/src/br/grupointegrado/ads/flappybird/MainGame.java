45af7310c5cb03e60ee8466ca765794fd2ba37fe
douglasamerico <douglas.alex.1212@gmail.com>
2015-11-16 21:01:48 -0200

    Implantação tela jogo

--- FlappyBird/core/src/br/grupointegrado/ads/flappybird/MainGame.java
+++ FlappyBird/core/src/br/grupointegrado/ads/flappybird/MainGame.java
@@ -1,6 +1,11 @@
 package br.grupointegrado.ads.flappybird;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainGame extends Game {
 
